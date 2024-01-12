c393b3a83ee80a9d7ac9a32ecbd4a618ecbc0433
Camilla Vedin <camilla.vedin@iths.se>
2024-01-12 15:14:27 +0100

    Done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,18 @@
 //applying global styles to my application
 import '@/app/ui/global.css';
+import { Metadata } from 'next';
 
 //importera font från google via filen font.ts adderar till body så att den används på hela webbplatsen
 import { inter } from '@/app/ui/fonts';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Camilla Dashboard',
+    default: 'Camilla Dahboard',
+  },
+  description: 'The official ...',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
